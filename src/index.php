9681baeab6b251e20968bd2c00874b2e1396f154
cherylau <clau136@my.bcit.ca>
2024-03-04 16:19:11 -0800

    Adding header and footer

--- src/index.php
+++ src/index.php
@@ -1,35 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
+include($_SERVER['DOCUMENT_ROOT'] . "/inc_header.php");
 
 
-  <form action="import/index.php" method="post" enctype="multipart/form-data">
-    Select CSV File to Upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload File" name="submit">
-  </form>
+?>
 
-  <button onclick="window.location.href='actions/display/display.php'">Display</button>
-  <button onclick="window.location.href='actions/register/register.php'">Register</button>
-  <button onclick="window.location.href='actions/login/login.php'">Login</button>
+
+<form action="import/index.php" method="post" enctype="multipart/form-data">
+  Select CSV File to Upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload File" name="submit">
+</form>
+
+<button onclick="window.location.href='actions/display/display.php'">Display</button>
+<button onclick="window.location.href='actions/register/register.php'">Register</button>
+<button onclick="window.location.href='actions/login/login.php'">Login</button>
 
 
 
-  <?php
-  // include './import/index.php';
-  // include './read/index.php';
 
 
-  ?>
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/inc_footer.php");
 
-
-</body>
-
-</html>
+?>
