316540059bcee9d07acbe1e8382d9e75e4d8a632
mikaem <mkmanaligod@gmail.com>
2024-03-05 00:23:54 -0800

    add invalid email or password error message

--- src/actions/login/login.php
+++ src/actions/login/login.php
@@ -1,18 +1,36 @@
 <?php include ('../../inc_header.php'); ?>
 
-<div class="container">
+<?php
+session_start(); // Start the session at the very beginning
+?>
+
+
     <h2>Login</h2>
+    <?php if (isset($_SESSION['error'])): ?>
+        <div class="alert alert-danger" role="alert">
+            <?= $_SESSION['error']; ?>
+            <?php unset($_SESSION['error']);?>
+        </div>
+    <?php endif; ?>
+
     <form action="login_process.php" method="post">
         <div class="form-group">
-            <label for="email">Email:</label>
+            <label for="email">Email address:</label>
             <input type="email" class="form-control" id="email" name="email" required>
         </div>
         <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" class="form-control" id="password" name="password" required>
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd" name="password" required>
         </div>
         <button type="submit" class="btn btn-primary">Login</button>
     </form>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+
 
 <?php include("../../inc_footer.php"); ?>
