78d47a26e82cbb6857d5f102eacf0543ccdcc939
cherylau <clau136@my.bcit.ca>
2024-03-04 16:13:03 -0800

    Importing csv

--- src/import/index.php
+++ src/import/index.php
@@ -6,11 +6,18 @@ if (isset($_POST["submit"])) {
   $handle = fopen($file, "r");
   if ($handle !== FALSE) {
     echo '<table border="1">';
+    echo '<tr><th>Date</th><th>Location</th><th>Credit</th><th>Debit</th><th>Total</th></tr>';
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+      echo '<tr>';
+      $num = count($data);
+      for ($c = 0; $c < $num; $c++) {
+        echo '<td>' . htmlspecialchars($data[$c]) . '</td>';
+      }
+      echo '</tr>';
+    }
     echo '</table>';
     fclose($handle);
   }
-
-
 
   if (move_uploaded_file($file, $destination)) {
     echo "The file has been uploaded.";
